1761e9329650812996aa0d9c2a1c1fd9e9fe7bb7
wamu_M <el.mail.de.wamu.m@gmail.com>
2023-03-14 21:11:42 +0100

    Doing compiler

--- compile.py
+++ compile.py
@@ -1,1 +1,14 @@
+sequence = input("Insert sequence following this pattern, '1;2;3;4'")
+sequence = sequence.split(";")
+
+def compile(S:list)-> str:
+    result = S[0]
+    for i in range(1,len(S)):
+        nullifier = [f"(n-{j})" for j in range(i)].join("") 
+        value = (execute(result)-S[i])/(i-2)
+        result = value+"*"+nullifier+f"+{result}"
+    return result
+
+def execute():
+    pass
 
