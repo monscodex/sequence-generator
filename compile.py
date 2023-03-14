6242327a7b8edf6dc4d8ca38c68031977caee07e
wamu_M <el.mail.de.wamu.m@gmail.com>
2023-03-14 21:39:05 +0100

    done create_formula

--- compile.py
+++ compile.py
@@ -1,15 +1,7 @@
-sequence = input("Insert sequence following this pattern, '1;2;3;4'")
-sequence = sequence.split(";")
-
-def compile(S:list)-> str:
+def create_formula(sequence:list)-> str:
     result = ""
-    for i in range(1,len(S)+1):
-        nullifier = "" # all the (n-1)(n-2)... 
-        X = ""# the coeficient of the nullifier
+    for i in range(1,len(sequence)+1):
+        nullifier = [f"(n-{j+1})" for j in range(i)].join("") # all the (n-1)(n-2)... 
+        coef = (sequence[i-1]-test_formula(result,i))/(test_formula(nullifier,i)# the coeficient of the nullifier
         result = f"{value}*{nullifier}+{result}"
     return result
-
-def execute():
-    pass
-
-print(compile(sequence))
