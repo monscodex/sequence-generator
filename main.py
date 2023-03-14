9af5c00591c4703fb75c78e8f44e1a23d564846c
monscodex <r2bs@protonmail.com>
2023-03-14 21:12:01 +0100

    Basic ideas for main.py and execute.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# Starts the program, while loop and prompts the user for terms in the formula
+# Every time the next term is entered the new formula is calculated and printed.
+
+# The user can then test the formula for all the past values (may be in a visual way??? -> Formula stays still and  the  n  is replaced with its value and dynamically calculated in the formula)
