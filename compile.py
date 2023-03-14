db5715a2a12bb9f3737768e88176d5316c39db78
wamu_M <el.mail.de.wamu.m@gmail.com>
2023-03-14 21:42:31 +0100

    fixed lil bug

--- compile.py
+++ compile.py
@@ -1,7 +1,8 @@
 def create_formula(sequence:list)-> str:
     result = ""
     for i in range(1,len(sequence)+1):
-        nullifier = [f"(n-{j+1})" for j in range(i)].join("") # all the (n-1)(n-2)... 
+        nullifier = [f"*(n-{j+1})" for j in range(i)].join("") # all the (n-1)(n-2)... 
         coef = (sequence[i-1]-test_formula(result,i))/(test_formula(nullifier,i)# the coeficient of the nullifier
-        result = f"{value}*{nullifier}+{result}"
+        result = f"{value}{nullifier}+{result}"
     return result
+                                                       
