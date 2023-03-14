03f6faf2f8b3673a0680a2b73826e6ae03cc3f57
monscodex <r2bs@protonmail.com>
2023-03-14 23:11:24 +0100

    First working version

--- compile.py
+++ compile.py
@@ -8,15 +8,6 @@ class SequenceTerm:
 
 
 def create_formula(sequence:list)-> str:
-<<<<<<< HEAD
-    result = ""
-    for i in range(1,len(sequence)+1):
-        nullifier = [f"*(n-{j+1})" for j in range(i)].join("") # all the (n-1)(n-2)... 
-        coef = (sequence[i-1]-test_formula(result,i))/(test_formula(nullifier,i)# the coeficient of the nullifier
-        result = f"{value}{nullifier}+{result}"
-    return result
-                                                       
-=======
     formula = ""
 
     for term_index, term_value in enumerate(sequence, start=1):
@@ -41,8 +32,3 @@ def update_existing_formula_with_next_term(formula: str, next_term: SequenceTerm
     ) / test_formula(nullifier, next_term.index)
 
     return f"{coef}*{nullifier}+{formula}"
-
-
-
-
->>>>>>> bfa2ec2 (First working version)
